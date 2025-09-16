eb9c10f90a0fac388000a7d1fe3b8e0def7802f8
Hwahyeon Yu <burdle1961@gmail.com>
2025-09-16 12:14:44 +0900

    Update

--- YOLO/4.type_of_source.py
+++ YOLO/4.type_of_source.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 from datetime import datetime
 
-model = YOLO ('d:/fish/runs/detect/train/weights/best.pt')
+model = YOLO ('E:/fish/runs/detect/train/weights/best.pt')
 
 # Stream mode로 예측
 
 print ("start prediction : ", datetime.now())
-result = model.predict(source='d:/fish/tropicalfishes.mp4', show=False, verbose=False, stream=True)
+result = model.predict(source='E:/fish/tropicalfishes.mp4', show=False, verbose=False, stream=True)
 print (type(result))        # <class 'generator'>
 print ("finish prediction : ", datetime.now())
 
@@ -18,7 +18,7 @@ for res in result :
 # Stream mode off로 예측
 # mp4 내의 모든 frame을 처리한 후에 result 값 반환 (영상이 긴 경우, memory 부족 발생 가능)
 print ("start prediction : ", datetime.now())
-result = model.predict(source='d:/fish/tropicalfishes.mp4', show=False, verbose=False, stream=False)
+result = model.predict(source='E:/fish/tropicalfishes.mp4', show=False, verbose=False, stream=False)
 print (type(result))    # <class 'list'>
 print ("finish prediction : ", datetime.now())
 
---
+++ openCV/3.OpenCV+YOLO.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+import cv2
+model = YOLO ('E:/fish/runs/detect/train/weights/best.pt')
+cap = cv2.VideoCapture('E:/fish/fish2.mp4')
+frameCount = 0
+while (True) :
+    ret, frame = cap.read()
+    if (not(ret)) : break
+    
+    frame = cv2.resize(frame, dsize=(640, 360))
+    result = model.predict(source=frame, show=True, verbose=False, stream=False, conf=0.7, imgsz=640)
+    res = result[0]  # cap.read()에서 한장의 이미지 프레임만 읽어 예측했기 때문에, result[0]
+
+    for box in res.boxes :
+        print(f"FrameCount = {frameCount}, {box.data.cpu().numpy()}")
+        npp = box.xyxy.cpu().numpy()
+        npcls = box.cls.cpu().numpy()
+        cx = int((npp[0][0]+npp[0][2])/2)
+        cy = int((npp[0][1]+npp[0][3])/2)
+        frame = cv2.circle(frame, (cx, cy),30, (0,255,255), 3)
+        cv2.putText(frame, res.names[npcls[0]] , org=(cx,cy), color=(0,255,0), fontScale=1, thickness=2, lineType = cv2.LINE_AA, fontFace=cv2.FONT_HERSHEY_SIMPLEX)
+        cv2.imshow('Detected Object', frame) 
+        if (cv2.waitKey(1) == 27) :
+            break
+        frameCount += 1
+cap.release()
+cv2.destroyAllWindows()
